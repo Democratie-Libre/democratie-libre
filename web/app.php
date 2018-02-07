c20c5507afb36765d0e4e21ec67fbe4912d57292
quantumm <david.bresteau@u-psud.fr>
2018-02-07 15:10:17 +0100

    Upgrade to Symfony 3.2

--- web/app.php
+++ web/app.php
@@ -3,7 +3,8 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require __DIR__.'/../app/autoload.php';
+include_once __DIR__.'/../var/bootstrap.php.cache';
 
 // Use APC for autoloading to improve performance.
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
